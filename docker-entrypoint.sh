0080ea23e2b072d6cd28c132c9a1cea2a6d3c35c
Norman Lorrain (lille) <normanlorrain@gmail.com>
2026-05-11 20:06:47 -0600

    Rebase

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-pip install -r requirements.txt
+# Sync dependencies (fast thanks to uv + cache)
+uv sync --frozen --no-dev   # or remove --no-dev if you want dev deps
+
+# Optional: make sure mkdocs is in PATH
+export PATH="/app/.venv/bin:$PATH"
 
 exec "$@"

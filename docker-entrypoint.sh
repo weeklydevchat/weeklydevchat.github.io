0b683a466941498e07f7d3fd90877e8142961ae9
Norman Lorrain (lille) <normanlorrain@gmail.com>
2026-05-26 18:17:54 -0600

    Removed unneeded comment.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
 
-# Sync dependencies (fast thanks to uv + cache)
 uv sync --frozen --no-dev   # or remove --no-dev if you want dev deps
 
 # Optional: make sure mkdocs is in PATH
